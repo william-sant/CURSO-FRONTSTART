6772bc525763b996ac536c3de8fc99bd741579b1
William Santana <81190663+william-sant@users.noreply.github.com>
2023-01-27 11:02:57 -0300

    .

---
+++ VUE/projeto-restaurant/restaurant/vue.config.js
@@ -0,0 +1,26 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true
+})
+
+module.exports = {
+  css: {
+      loaderOptions: {
+          less: {
+              additionalData: `@import "@/assets/css/variables.less";`
+          }
+      }
+  },
+  chainWebpack: (config) => {
+      const svgRule = config.module.rule('svg');
+
+      svgRule.uses.clear();
+
+      svgRule
+          .use('babel-loader')
+          .loader('babel-loader')
+          .end()
+          .use('vue-svg-loader')
+          .loader('vue-svg-loader');
+  }
+};
